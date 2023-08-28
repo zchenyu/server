8adc7ace9e8b66017f65554de52c316d15fb5620
Misha Chornyi <mchornyi@nvidia.com>
2023-08-28 12:50:13 -0700

    Update ORT version

--- build.py
+++ build.py
@@ -70,7 +70,7 @@ TRITON_VERSION_MAP = {
     "2.38.0dev": (
         "23.09dev",  # triton container
         "23.08",  # upstream container
-        "1.15.1",  # ORT
+        "1.16.0",  # ORT
         "2023.0.0",  # ORT OpenVINO
         "2023.0.0",  # Standalone OpenVINO
         "2.4.7",  # DCGM version
